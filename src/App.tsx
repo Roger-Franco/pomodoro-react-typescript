74121e7b12edf15756b4f3e98786c1519c92d52a
Roger-Franco <rogerfrancogalt@gmail.com>
2022-08-30 11:37:53 -0400

    Convertendo segundos em tempo (SecondsToTime)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <h1>Olá mundo!</h1>
-      <PomodoroTimer defaultPomodoroTime={1500} />
+      <PomodoroTimer defaultPomodoroTime={3660} />
     </div>
   );
 }
